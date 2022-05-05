ffafaad0fd2cade8ae08378785cacc3f053fd2f0
mohive <mohiuddin.mohive@gmail.com>
2022-05-05 23:18:52 +0600

    first commit

---
+++ src/component/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div>
+            <h3>order Summary </h3>
+               {/* <p>Selected Items:{cart.length}</p> */}
+        </div>
+    );
+};
+
+export default Cart;
